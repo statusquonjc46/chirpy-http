10c02a963539557fa12dd3cca67417a6e9e9aa36
statusquonjc46 <nicholas.coletta@gmail.com>
2025-05-19 21:52:08 -0400

    added middleware for counting hits, /metrics and /reset endpoints

--- main.go
+++ main.go
@@ -3,27 +3,60 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"sync/atomic"
 )
 
+// Server Health Function
 func HealthHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte("OK"))
 }
 
+func (cfg *apiConfig) metricHandler(w http.ResponseWriter, r *http.Request) {
+	hits := fmt.Sprintf("Hits: %d", cfg.fileserverHits.Load())
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(hits))
+
+}
+
+func (cfg *apiConfig) resetHandler(w http.ResponseWriter, r *http.Request) {
+	cfg.fileserverHits.Store(0)
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("OK"))
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
 func main() {
+	//vars
 	mux := http.NewServeMux()
 	server := &http.Server{
 		Addr:    ":8080",
 		Handler: mux,
 	}
+	cfg := &apiConfig{}
 
 	fmt.Printf("Attempting to serve at: %s", server.Addr)
 
-	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
+	//connection handlers
+	mux.Handle("/app/", cfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(".")))))
 	mux.HandleFunc("/healthz", HealthHandler)
+	mux.HandleFunc("/metrics", cfg.metricHandler)
+	mux.HandleFunc("/reset", cfg.resetHandler)
+	//Serve content on connection
 	err := server.ListenAndServe()
-
 	if err != nil {
 		fmt.Printf("Failed at ListenAndServe: %s", err)
 	}
